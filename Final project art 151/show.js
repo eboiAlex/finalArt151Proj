026f166587b84cf07683dd5cb29236185680bf1d
eboiAlex <97483265+eboiAlex@users.noreply.github.com>
2022-05-04 01:26:09 -0500

    Add files via upload

---
+++ Final project art 151/show.js
@@ -0,0 +1,19 @@
+// JavaScript Document
+var slideimages=new Array()
+var slidelinks=new Array()
+function slideshowimages(){
+for (i=0;i<slideshowimages.arguments.length;i++){
+slideimages[i]=new Image()
+slideimages[i].src=slideshowimages.arguments[i]
+}
+}
+
+
+
+function gotoshow(){
+if (!window.winslide||winslide.closed)
+winslide=window.open(slidelinks[whichlink])
+else
+winslide.location=slidelinks[whichlink]
+winslide.focus()
+}
