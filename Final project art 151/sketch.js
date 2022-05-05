f6c749b27aef68bc4e1bf6d222c8ff844e61bff9
eboiAlex <97483265+eboiAlex@users.noreply.github.com>
2022-05-04 21:42:05 -0500

    Delete Final project art 151 directory

--- Final project art 151/sketch.js
+++
@@ -1,8 +0,0 @@
-function setup() {
-  var canvas = createCanvas(100, 100);
-
-  // Move the canvas so it's inside our <div id="sketch-holder">.
-  canvas.parent('sketch-holder');
-
-  background(255, 0, 200);
-}
